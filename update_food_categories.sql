92dc7eb3578aeea96337d596d32899c3d3468f95
skalades <127845035+skalades@users.noreply.github.com>
2026-05-28 01:50:09 +0700

    Add update SQL script for food items categories

---
+++ update_food_categories.sql
@@ -0,0 +1,142 @@
+-- SQL script to update food_items categories
+-- Generated on 2026-05-27 20:49:59
+
+UPDATE `food_items` SET `category` = 'Karbohidrat' WHERE `id` = 306; -- Nasi Briyani
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 307; -- Ayam Masak Merah
+UPDATE `food_items` SET `category` = 'Protein Nabati' WHERE `id` = 308; -- Tempe Bumbu Kari
+UPDATE `food_items` SET `category` = 'Sayuran' WHERE `id` = 309; -- Tumis Futren
+UPDATE `food_items` SET `category` = 'Buah' WHERE `id` = 310; -- Lengkeng
+UPDATE `food_items` SET `category` = 'Karbohidrat' WHERE `id` = 311; -- Nasi Putih
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 312; -- telur fuyunghai
+UPDATE `food_items` SET `category` = 'Sayuran' WHERE `id` = 313; -- Cah Sayur
+UPDATE `food_items` SET `category` = 'Protein Nabati' WHERE `id` = 314; -- Tahu Jabrik
+UPDATE `food_items` SET `category` = 'Buah' WHERE `id` = 315; -- Apel
+UPDATE `food_items` SET `category` = 'Lainnya' WHERE `id` = 316; -- Bolu Kemasan padimas
+UPDATE `food_items` SET `category` = 'Lainnya' WHERE `id` = 317; -- Susu Greenfields full cream
+UPDATE `food_items` SET `category` = 'Karbohidrat' WHERE `id` = 318; -- Nasi Goreng Merah
+UPDATE `food_items` SET `category` = 'Protein Nabati' WHERE `id` = 319; -- tempe
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 320; -- bola daging
+UPDATE `food_items` SET `category` = 'Sayuran' WHERE `id` = 321; -- Lalapan
+UPDATE `food_items` SET `category` = 'Karbohidrat' WHERE `id` = 322; -- Nasi Uduk
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 323; -- telor bumbu bali
+UPDATE `food_items` SET `category` = 'Buah' WHERE `id` = 324; -- Anggur
+UPDATE `food_items` SET `category` = 'Protein Nabati' WHERE `id` = 325; -- Tahu Goreng
+UPDATE `food_items` SET `category` = 'Sayuran' WHERE `id` = 326; -- Wortel Buncis Bawang Putih
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 327; -- Ayam Bakar
+UPDATE `food_items` SET `category` = 'Buah' WHERE `id` = 328; -- Jeruk
+UPDATE `food_items` SET `category` = 'Protein Nabati' WHERE `id` = 329; -- Tahu kecap
+UPDATE `food_items` SET `category` = 'Karbohidrat' WHERE `id` = 330; -- Roti Kemasan merk Papabear
+UPDATE `food_items` SET `category` = 'Lainnya' WHERE `id` = 331; -- Kue Pia 100
+UPDATE `food_items` SET `category` = 'Lainnya' WHERE `id` = 332; -- Regal Marrie
+UPDATE `food_items` SET `category` = 'Lainnya' WHERE `id` = 333; -- Susu Milk Life
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 334; -- Telor Rebus
+UPDATE `food_items` SET `category` = 'Lainnya' WHERE `id` = 335; -- Susu Frisian Flag 125ml
+UPDATE `food_items` SET `category` = 'Karbohidrat' WHERE `id` = 336; -- Roti  Abon UMKM
+UPDATE `food_items` SET `category` = 'Karbohidrat' WHERE `id` = 337; -- Roti Srikaya UMKM
+UPDATE `food_items` SET `category` = 'Lainnya' WHERE `id` = 338; -- Brownies Kukus
+UPDATE `food_items` SET `category` = 'Lainnya' WHERE `id` = 339; -- Susu Ultra Milk 125 ml
+UPDATE `food_items` SET `category` = 'Lainnya' WHERE `id` = 340; -- Biskuit Sari Gandum
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 341; -- Ayam Ungkep Paha
+UPDATE `food_items` SET `category` = 'Protein Nabati' WHERE `id` = 342; -- Tempe Ungkep
+UPDATE `food_items` SET `category` = 'Karbohidrat' WHERE `id` = 343; -- Roti Isi Bulat Coklat UMKM
+UPDATE `food_items` SET `category` = 'Buah' WHERE `id` = 344; -- Pisang Muli
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 345; -- Dimsum ayam
+UPDATE `food_items` SET `category` = 'Karbohidrat' WHERE `id` = 346; -- Roti Isi Selai Coklat
+UPDATE `food_items` SET `category` = 'Lainnya' WHERE `id` = 347; -- Puding Susu
+UPDATE `food_items` SET `category` = 'Lainnya' WHERE `id` = 348; -- Donat Kampung
+UPDATE `food_items` SET `category` = 'Buah' WHERE `id` = 349; -- Kurma
+UPDATE `food_items` SET `category` = 'Lainnya' WHERE `id` = 350; -- Bolu Pisang
+UPDATE `food_items` SET `category` = 'Lainnya' WHERE `id` = 351; -- Biskuit Sari Gandum coklat
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 352; -- Telur Rebus
+UPDATE `food_items` SET `category` = 'Karbohidrat' WHERE `id` = 353; -- Roti Oles Coklat
+UPDATE `food_items` SET `category` = 'Lainnya' WHERE `id` = 354; -- Bolu Gulung
+UPDATE `food_items` SET `category` = 'Karbohidrat' WHERE `id` = 355; -- CocoKrunch
+UPDATE `food_items` SET `category` = 'Buah' WHERE `id` = 356; -- Buah Pear
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 357; -- Baso Ayam
+UPDATE `food_items` SET `category` = 'Buah' WHERE `id` = 358; -- Buah Naga
+UPDATE `food_items` SET `category` = 'Protein Nabati' WHERE `id` = 359; -- Kacang Bandung
+UPDATE `food_items` SET `category` = 'Lainnya' WHERE `id` = 360; -- Bolu Kukus
+UPDATE `food_items` SET `category` = 'Lainnya' WHERE `id` = 361; -- Brownies UMKM
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 362; -- Rolade Ayam
+UPDATE `food_items` SET `category` = 'Protein Nabati' WHERE `id` = 363; -- Kacang Koro
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 364; -- Chiken Katsu
+UPDATE `food_items` SET `category` = 'Karbohidrat' WHERE `id` = 365; -- Roti Burger
+UPDATE `food_items` SET `category` = 'Buah' WHERE `id` = 366; -- Pisang Cavendi
+UPDATE `food_items` SET `category` = 'Karbohidrat' WHERE `id` = 367; -- Roti Kasino
+UPDATE `food_items` SET `category` = 'Lainnya' WHERE `id` = 368; -- Susu Ultra Mimi 125 ml
+UPDATE `food_items` SET `category` = 'Protein Nabati' WHERE `id` = 369; -- Selai Kacang
+UPDATE `food_items` SET `category` = 'Protein Nabati' WHERE `id` = 370; -- Tempe Orek
+UPDATE `food_items` SET `category` = 'Karbohidrat' WHERE `id` = 371; -- Roti Tawar UMKM
+UPDATE `food_items` SET `category` = 'Lainnya' WHERE `id` = 372; -- Susu Sekolah
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 373; -- Telur Mentah
+UPDATE `food_items` SET `category` = 'Lainnya' WHERE `id` = 374; -- Susu Ultra Milk 1 Liter
+UPDATE `food_items` SET `category` = 'Karbohidrat' WHERE `id` = 375; -- Sari Roti milky soft
+UPDATE `food_items` SET `category` = 'Buah' WHERE `id` = 376; -- Apel Fuji
+UPDATE `food_items` SET `category` = 'Karbohidrat' WHERE `id` = 377; -- Nasi Daun Pandan
+UPDATE `food_items` SET `category` = 'Sayuran' WHERE `id` = 378; -- Steam Wortel Buncis
+UPDATE `food_items` SET `category` = 'Sayuran' WHERE `id` = 379; -- Sup Kimlo
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 380; -- Telur Puyuh Pindang
+UPDATE `food_items` SET `category` = 'Karbohidrat' WHERE `id` = 381; -- Kentang Goreng
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 382; -- Chiken Stik
+UPDATE `food_items` SET `category` = 'Sayuran' WHERE `id` = 383; -- Mix Vegetable
+UPDATE `food_items` SET `category` = 'Lainnya' WHERE `id` = 384; -- Mushroom Sauce
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 385; -- Telur okonomiyaki
+UPDATE `food_items` SET `category` = 'Protein Nabati' WHERE `id` = 386; -- Pepes Tahu
+UPDATE `food_items` SET `category` = 'Sayuran' WHERE `id` = 387; -- Salad coleslow
+UPDATE `food_items` SET `category` = 'Buah' WHERE `id` = 388; -- Semangka
+UPDATE `food_items` SET `category` = 'Karbohidrat' WHERE `id` = 389; -- Nasi Goreng
+UPDATE `food_items` SET `category` = 'Sayuran' WHERE `id` = 390; -- Brokoli Bawang Putih
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 391; -- Telur Iris
+UPDATE `food_items` SET `category` = 'Buah' WHERE `id` = 392; -- Melon
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 393; -- Ayam Goreng Bawang Putih
+UPDATE `food_items` SET `category` = 'Sayuran' WHERE `id` = 394; -- Tumis Labu Siam
+UPDATE `food_items` SET `category` = 'Lainnya' WHERE `id` = 395; -- Kerupuk Udang Kemasan
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 396; -- Nugget Ayam
+UPDATE `food_items` SET `category` = 'Protein Nabati' WHERE `id` = 397; -- Tahu Saos Bangkok
+UPDATE `food_items` SET `category` = 'Protein Nabati' WHERE `id` = 398; -- Oseng Tempe
+UPDATE `food_items` SET `category` = 'Sayuran' WHERE `id` = 399; -- Tumis Labu Wortel
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 400; -- Telur Balado
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 401; -- Sate Ayam
+UPDATE `food_items` SET `category` = 'Karbohidrat' WHERE `id` = 402; -- Nasi Kuning Tanpa Santan
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 403; -- Telur Ceplok
+UPDATE `food_items` SET `category` = 'Buah' WHERE `id` = 404; -- Strawberry
+UPDATE `food_items` SET `category` = 'Karbohidrat' WHERE `id` = 405; -- Potato Wedges
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 406; -- Dimsum Mentai
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 407; -- Telur Kukus
+UPDATE `food_items` SET `category` = 'Sayuran' WHERE `id` = 408; -- Pakcoy Garlic
+UPDATE `food_items` SET `category` = 'Protein Nabati' WHERE `id` = 409; -- Tahu Crispy
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 410; -- Rendang Ayam Baby Potato
+UPDATE `food_items` SET `category` = 'Protein Nabati' WHERE `id` = 411; -- Keripik Tempe
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 412; -- Rolade Telur
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 413; -- Lele Crispy
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 414; -- Tumis Baso
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 415; -- Ayam Goreng Lengkuas
+UPDATE `food_items` SET `category` = 'Protein Nabati' WHERE `id` = 416; -- Tahu Oseng
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 417; -- Chiken Steak
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 418; -- Ayam Goreng
+UPDATE `food_items` SET `category` = 'Karbohidrat' WHERE `id` = 419; -- Kimbab
+UPDATE `food_items` SET `category` = 'Sayuran' WHERE `id` = 420; -- Salad Sayur
+UPDATE `food_items` SET `category` = 'Protein Nabati' WHERE `id` = 421; -- Edamame
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 422; -- Telur Dadar
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 423; -- Pepes Ayam
+UPDATE `food_items` SET `category` = 'Protein Nabati' WHERE `id` = 424; -- Tempe Mendoan
+UPDATE `food_items` SET `category` = 'Karbohidrat' WHERE `id` = 425; -- Lontong
+UPDATE `food_items` SET `category` = 'Protein Nabati' WHERE `id` = 426; -- Bumbu Kacang
+UPDATE `food_items` SET `category` = 'Lainnya' WHERE `id` = 427; -- Keju Slice
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 428; -- Patty Ayam
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 429; -- Chiken Katsu
+UPDATE `food_items` SET `category` = 'Sayuran' WHERE `id` = 430; -- Wortel Kentang Curry
+UPDATE `food_items` SET `category` = 'Protein Nabati' WHERE `id` = 431; -- Kering Tempe Daun Jeruk
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 432; -- Semur Telur
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 433; -- Ekado Telur Puyuh
+UPDATE `food_items` SET `category` = 'Lainnya' WHERE `id` = 434; -- Miso Sop
+UPDATE `food_items` SET `category` = 'Sayuran' WHERE `id` = 435; -- Salad Hokben
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 436; -- Chiken Bulgogi
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 437; -- Telur Ceplok Saus Singapur
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 438; -- Beef Slice Teriaki
+UPDATE `food_items` SET `category` = 'Protein Nabati' WHERE `id` = 439; -- Tempe Crispy
+UPDATE `food_items` SET `category` = 'Sayuran' WHERE `id` = 440; -- Tumis Sawi Wortel
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 441; -- Chiken Cheese Ball Crumble
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 442; -- Dorry Crispy
+UPDATE `food_items` SET `category` = 'Lainnya' WHERE `id` = 443; -- Susu Diamond
+UPDATE `food_items` SET `category` = 'Protein Hewani' WHERE `id` = 444; -- Telur Bumbu Bali
